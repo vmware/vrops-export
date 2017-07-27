fccb03d00d692c66566ecde2c261044772a2245d
pontus@rydin@gmail.com <pontus@rydin.nu>
2017-07-27 14:23:02 -0400

    Updated copyright

--- src/bin/exporttool.bat
+++ src/bin/exporttool.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-REM Copyright 2017 Pontus Rydin
+REM Copyright 2017 VMware, Inc. All Rights Reserved.
 REM
 REM Licensed under the Apache License, Version 2.0 (the "License");
 REM you may not use this file except in compliance with the License.
